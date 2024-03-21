40e573ae2888ed99cf3067d2acb50204a74a53ab
Matthew Denune <mattdenune@gmail.com>
2024-03-21 17:05:22 -0400

    updated server.js and created .env

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-console.log('Welcome to server')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = 8000;
+
+const app = express();
+
+app.listen(port, () => console.log(`Server listening on ${port}`));
